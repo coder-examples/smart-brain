613bb98a544174144d59f2dc79735f96183d9ddb
Aayush Shah <visionmoneymantra.web@gmail.com>
2020-01-09 18:29:35 +0530

    Adding Files...

---
+++ precache-manifest.deb88e7b97108b3c0f79e9a0b48f36dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9427ca090d28d6746681220c282c5713",
+    "url": "/coder-examples/smart-brain/index.html"
+  },
+  {
+    "revision": "2653da3eb5cc7118502f",
+    "url": "/coder-examples/smart-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b6361aa89af1765debff",
+    "url": "/coder-examples/smart-brain/static/css/main.8967b8f4.chunk.css"
+  },
+  {
+    "revision": "2653da3eb5cc7118502f",
+    "url": "/coder-examples/smart-brain/static/js/2.acc39484.chunk.js"
+  },
+  {
+    "revision": "b6361aa89af1765debff",
+    "url": "/coder-examples/smart-brain/static/js/main.7ef4f6e4.chunk.js"
+  },
+  {
+    "revision": "d081a7c6e29356a6daa6",
+    "url": "/coder-examples/smart-brain/static/js/runtime~main.4c31aad7.js"
+  }
+]);
