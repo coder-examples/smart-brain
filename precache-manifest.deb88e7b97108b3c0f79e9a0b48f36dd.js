f77d951000823118f93883dcf6bd9f0c95da86a3
coder-examples <51196567+coder-examples@users.noreply.github.com>
2020-01-10 15:01:17 +0530

    Update precache-manifest.deb88e7b97108b3c0f79e9a0b48f36dd.js

--- precache-manifest.deb88e7b97108b3c0f79e9a0b48f36dd.js
+++ precache-manifest.deb88e7b97108b3c0f79e9a0b48f36dd.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9427ca090d28d6746681220c282c5713",
-    "url": "/coder-examples/smart-brain/index.html"
+    "revision": "a0f24ae5725b234426bb88c2d905a4ae",
+    "url": "./index.html"
   },
   {
-    "revision": "2653da3eb5cc7118502f",
-    "url": "/coder-examples/smart-brain/static/css/2.eb45ad44.chunk.css"
+    "revision": "a701ad388ca13549f53c",
+    "url": "./static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "b6361aa89af1765debff",
-    "url": "/coder-examples/smart-brain/static/css/main.8967b8f4.chunk.css"
+    "revision": "3eeace5a7705f2ddfc92",
+    "url": "./static/css/main.8967b8f4.chunk.css"
   },
   {
-    "revision": "2653da3eb5cc7118502f",
-    "url": "/coder-examples/smart-brain/static/js/2.acc39484.chunk.js"
+    "revision": "a701ad388ca13549f53c",
+    "url": "./static/js/2.28357ad9.chunk.js"
   },
   {
-    "revision": "b6361aa89af1765debff",
-    "url": "/coder-examples/smart-brain/static/js/main.7ef4f6e4.chunk.js"
+    "revision": "3eeace5a7705f2ddfc92",
+    "url": "./static/js/main.08b0071d.chunk.js"
   },
   {
-    "revision": "d081a7c6e29356a6daa6",
-    "url": "/coder-examples/smart-brain/static/js/runtime~main.4c31aad7.js"
+    "revision": "cd91da34cfd62d004fe9",
+    "url": "./static/js/runtime~main.ce511409.js"
   }
 ]);
